274f948268bf7f24b9dc7302559498e079a0b292
farsidehnavi <154635498+farsidehnavi@users.noreply.github.com>
2024-06-14 14:18:35 +0330

    a bug fixed, 1.0.1

--- login.js
+++ login.js
@@ -1,5 +1,24 @@
 window.onresize=()=>Resized()
+let Situation=true
+// PC=true
+// Mobile=false
 function Resized(){
-    console.log('width='+window.innerWidth)
-    console.log('height='+window.innerHeight)
+    if((window.innerHeight>window.innerWidth)&&(Situation)){
+        ResizeToMobile()
+    }
+    if((window.innerHeight<window.innerWidth)&&(!Situation)){
+        ResizeToPC()
+    }
+}
+function ResizeToMobile(){
+    document.querySelector('.dlogin').classList.add('dlogin-mobile')
+    document.querySelector('.bcorner').classList.add('bcorner-mobile')
+    document.querySelector('.wcorner').classList.add('wcorner-mobile')
+    Situation=false
+}
+function ResizeToPC(){
+    document.querySelector('.dlogin').classList.remove('dlogin-mobile')
+    document.querySelector('.bcorner').classList.remove('bcorner-mobile')
+    document.querySelector('.wcorner').classList.remove('wcorner-mobile')
+    Situation=true
 }
