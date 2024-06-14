8f4975bc08be486f52360afc7d9755ec8077c476
farsidehnavi <154635498+farsidehnavi@users.noreply.github.com>
2024-06-14 12:08:36 +0330

    Login page and Welcome page are on the way,1.0.0

---
+++ login.js
@@ -0,0 +1,5 @@
+window.onresize=()=>Resized()
+function Resized(){
+    console.log('width='+window.innerWidth)
+    console.log('height='+window.innerHeight)
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+document.querySelector('button').addEventListener('click',()=>OpenUp())
+function OpenUp(){
+    // window.open('index2.html','','height=700,width=1000')
+    window.open('login.html','','height=700,width=1000')
+}
