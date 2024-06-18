de022285a2892b0e66becd3eb8d3e997b2b45153
farsidehnavi <154635498+farsidehnavi@users.noreply.github.com>
2024-06-18 19:11:34 +0330

    2.0.0

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 document.querySelector('button').addEventListener('click',()=>OpenUp())
 function OpenUp(){
     // window.open('index2.html','','height=700,width=1000')
-    window.open('login.html','','height=700,width=1000')
+    // window.open('login.html','','height=700,width=1000')
+    window.open('order.html','','height=700,width=400')
 }
