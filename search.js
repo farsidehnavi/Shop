970bddbce798d950e166b36c38f4f3fecad1ce3e
farsidehnavi <154635498+farsidehnavi@users.noreply.github.com>
2024-07-09 21:10:03 +0330

    4.0.0

--- search.js
+++ search.js
@@ -1,3 +1,5 @@
+// CODES FOR DESINE OF PHONES !
+
 setInterval(()=>RefreshTime(),500)
 function RefreshTime(){
     document.querySelectorAll('.time').forEach(v=>v.innerText=Date().split(' ',5)[4])
@@ -13,20 +15,6 @@ function RefreshBatteryPersentage(){
         document.querySelectorAll('.battery-percentage').forEach(v=>v.innerText=String(battery.level*100).split('.',1)+'%')
     })
 }
-let IsScreenOn=[true,true,true,true,true,true]
-a=[1,2,3,4,5,6]
-a.forEach(v=>document.querySelector('.p'+v).addEventListener('click',()=>{
-    if(IsScreenOn[v-1]){
-        document.querySelectorAll('.tf'+v).forEach(v=>v.classList.add('tf-off'))
-        document.querySelectorAll('.i'+v).forEach(v=>v.classList.add('i-off'))
-        IsScreenOn[v-1]=false
-    }
-    else {
-        document.querySelectorAll('.tf'+v).forEach(v=>v.classList.remove('tf-off'))
-        document.querySelectorAll('.i'+v).forEach(v=>v.classList.remove('i-off'))
-        IsScreenOn[v-1]=true
-    }
-}))
 RefreshDate()
 function RefreshDate(){
     document.querySelectorAll('.date').forEach(v=>v.innerText=Date().slice(0,15))
@@ -37,37 +25,29 @@ if(Date().split(' ',5)[4].split(':',1)[0]==23){
 else{
     setInterval(()=>RefreshDate(),3600000)
 }
+
 // CODES FOR RESPONSIVE PAGE !
+
 window.onresize=()=>RefreshPageGrid()
 RefreshPageGrid()
 function RefreshPageGrid(){
     switch (true){
-        case(window.innerWidth<window.innerHeight/1.05):
-            // document.querySelector('.phones').style.gridTemplateColumns='80vw'
-            document.querySelector('.phones').style.gridTemplateColumns=window.innerHeight*0.45+'px'
+        case(document.querySelector('.right-section').clientWidth<document.querySelector('.right-section').clientHeight/1.05):
+            document.querySelector('.phones').style.gridTemplateColumns=document.querySelector('.right-section').clientHeight*0.45+'px'
             UpperMenuOnlyIconON()
-            // alert(1)
             break
-        case(window.innerWidth<window.innerHeight*1.46):
-            // document.querySelector('.phones').style.gridTemplateColumns='35vw 35vw'
-            document.querySelector('.phones').style.gridTemplateColumns=window.innerHeight*0.45+'px '+window.innerHeight*0.45+'px'
+        case(document.querySelector('.right-section').clientWidth<document.querySelector('.right-section').clientHeight*1.46):
+            document.querySelector('.phones').style.gridTemplateColumns=document.querySelector('.right-section').clientHeight*0.45+'px '+document.querySelector('.right-section').clientHeight*0.45+'px'
             UpperMenuOnlyIconOff()
-            // alert(2)
             break
-        case(window.innerWidth<window.innerHeight*2):
-            // document.querySelector('.phones').style.gridTemplateColumns='25vw 25vw 25vw'
-            document.querySelector('.phones').style.gridTemplateColumns=window.innerHeight*0.45+'px '+window.innerHeight*0.45+'px '+window.innerHeight*0.45+'px'
-            // alert(3)
+        case(document.querySelector('.right-section').clientWidth<document.querySelector('.right-section').clientHeight*2):
+            document.querySelector('.phones').style.gridTemplateColumns=document.querySelector('.right-section').clientHeight*0.45+'px '+document.querySelector('.right-section').clientHeight*0.45+'px '+document.querySelector('.right-section').clientHeight*0.45+'px'
             break
-        case(window.innerWidth<window.innerHeight*2.5):
-            // document.querySelector('.phones').style.gridTemplateColumns='23vw 23vw 23vw 23vw'
-            document.querySelector('.phones').style.gridTemplateColumns=window.innerHeight*0.45+'px '+window.innerHeight*0.45+'px '+window.innerHeight*0.45+'px '+window.innerHeight*0.45+'px'
-            // alert(4)
+        case(document.querySelector('.right-section').clientWidth<document.querySelector('.right-section').clientHeight*2.5):
+            document.querySelector('.phones').style.gridTemplateColumns=document.querySelector('.right-section').clientHeight*0.45+'px '+document.querySelector('.right-section').clientHeight*0.45+'px '+document.querySelector('.right-section').clientHeight*0.45+'px '+document.querySelector('.right-section').clientHeight*0.45+'px'
             break
-        case(window.innerWidth<window.innerHeight*3):
-            // document.querySelector('.phones').style.gridTemplateColumns='18vw 18vw 18vw 18vw 18vw'
-            document.querySelector('.phones').style.gridTemplateColumns=window.innerHeight*0.45+'px '+window.innerHeight*0.45+'px '+window.innerHeight*0.45+'px '+window.innerHeight*0.45+'px '+window.innerHeight*0.45+'px'
-            // alert(5)
+        case(document.querySelector('.right-section').clientWidth<document.querySelector('.right-section').clientHeight*3):
+            document.querySelector('.phones').style.gridTemplateColumns=document.querySelector('.right-section').clientHeight*0.45+'px '+document.querySelector('.right-section').clientHeight*0.45+'px '+document.querySelector('.right-section').clientHeight*0.45+'px '+document.querySelector('.right-section').clientHeight*0.45+'px '+document.querySelector('.right-section').clientHeight*0.45+'px'
             break
     }
 }
@@ -78,4 +58,81 @@ function UpperMenuOnlyIconON(){
 function UpperMenuOnlyIconOff(){
     document.querySelectorAll('.pmenu-item').forEach(v=>v.classList.remove('pmenu-item-onlyicon'))
     document.querySelectorAll('.menu-item').forEach(v=>v.classList.remove('menu-item-onlyicon'))
+}
+
+// BACKEND !
+
+let IsLeftSectionOpen=false
+function OpenLeftSection(){
+    document.querySelector('.left-section').style.display='block'
+    document.querySelector('.right-section').classList.add('right-section-leftSectionOpened')
+    RefreshPageGrid()
+    IsLeftSectionOpen=true
+}
+function CloseLeftSection(){
+    document.querySelector('.left-section').style.display='none'
+    document.querySelector('.right-section').classList.remove('right-section-leftSectionOpened')
+    RefreshPageGrid()
+    IsLeftSectionOpen=false
+}
+function TurnOnPhoneScreen(){
+    document.querySelectorAll('.tf'+v).forEach(v=>v.classList.add('tf-off'))
+    document.querySelectorAll('.i'+v).forEach(v=>v.classList.add('i-off'))
+}
+function TurnOffPhoneScreen(){
+    document.querySelectorAll('.tf'+v).forEach(v=>v.classList.remove('tf-off'))
+    document.querySelectorAll('.i'+v).forEach(v=>v.classList.remove('i-off'))
+}
+// let IsScreenOn=[true,true,true,true,true,true]
+// a=[0,1,2,3,4,5]
+// a.forEach(v=>document.querySelector('.p'+v).addEventListener('click',()=>{
+//     if(IsScreenOn[v]){
+//         TurnOnPhoneScreen()
+//         IsScreenOn[v]=false
+//         OpenLeftSection()
+//     }
+//     else {
+//         TurnOffPhoneScreen()
+//         IsScreenOn[v]=true
+//         CloseLeftSection()
+//     }
+// }))
+const OrderHard=[
+    ['a.png','MakeUp Brush'],
+    ['b11.png','MakeUp Brush type 2'],
+    ['brush-3.png','MakeUp Brush type 3']
+]
+i=0
+do{
+    document.querySelector('.phones').innerHTML+=`<div class="phone p${i}">
+    <div class="notification-bar-full">
+    <p class="time i${i}"></p>
+    <div class="enotification-bar"></div>
+    <div class="notification-bar">
+    <p class="time-off tf${i}"></p>
+    <img class="wifi i${i}" src="icons/wifi/wifi-white.svg">
+    <p class="battery-percentage i${i}"></p>
+    <img class="ibattery i${i}" src="icons/battery/battery-white.svg">
+    </div>
+    </div>
+    <img class="iphone i${i}" src="icons/${OrderHard[i][0]}">
+    <p class="order-name i${i}">${OrderHard[i][1]}</p>
+    <p class="order-name date tf0"></p>
+    <hr class="right-hr">
+    </div>`
+    // document.querySelector('.p'+i).addEventListener('click',()=>{ItemClicked(i)})
+    i++
+}while(i<OrderHard.length)
+i=0
+do{
+    document.querySelector('.phones').innerHTML+='<div class="ephones"></div>'
+    i++
+}while(i<3)
+document.querySelectorAll('.phone').forEach((v,k)=>v.addEventListener('click',()=>ItemClicked(k)))
+function ItemClicked(who){
+    if(!IsLeftSectionOpen){
+        OpenLeftSection()
+    }
+    document.querySelector('.iorder').src='icons/'+OrderHard[who][0]
+    document.querySelector('.fp').innerText=OrderHard[who][1]
 }
--- test.js
+++ test.js
@@ -1,8 +1,16 @@
 document.querySelector('button').addEventListener('click',()=>OpenUp())
+document.querySelector('.b').addEventListener('click',()=>OpenUp2())
 function OpenUp(){
     // window.open('index2.html','','height=700,width=1000')
     // window.open('login.html','','height=700,width=1000')
     // window.open('order.html','','height=700,width=400')
     window.open('search.html','','height=700,width=1200')
+    // window.open('test2.html','','height=700,width=1200')
+}
+function OpenUp2(){
+    // window.open('index2.html','','height=700,width=1000')
+    // window.open('login.html','','height=700,width=1000')
+    window.open('order.html','','height=700,width=400')
+    // window.open('search.html','','height=700,width=1200')
     // window.open('test2.html','','height=700,width=1200')
 }
